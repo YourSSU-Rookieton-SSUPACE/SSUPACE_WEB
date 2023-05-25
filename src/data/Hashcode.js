abaf67dda055341e1b51679e786ca24d5025c424
JjungminLee <julie0964@naver.com>
2023-05-25 17:13:41 +0900

    feat : data 양식 지정

---
+++ src/data/Hashcode.js
@@ -0,0 +1,27 @@
+export const SpaceUsage={
+    QUIET:"소근소근",
+    LAPTOP:"노트북 존",
+    STUDY:"열공모드",
+    STUDYROOM:"스터디룸",
+    COMMUNITY:"커뮤니티존",
+    CHARGE:"충전가능",
+
+    
+};
+
+export const LaptopUsage={
+    LAPTOP_TRUE:"노트북 사용해도 돼요!",
+    LAPTIP_FALSE:"노트북 사용하면 안돼요!",
+
+
+};
+
+export const ReserveUsage={
+    RESERVE_TRUE:"예약이 필요해요!",
+    RESERVE_FALSE:"예약 안해도 돼요!"
+
+}
+
+Object.freeze(SpaceUsage);
+Object.freeze(LaptopUsage);
+Object.freeze(ReserveUsage);
---
+++ src/data/SpaceData.js
@@ -0,0 +1,19 @@
+import { LaptopUsage, ReserveUsage } from "./Hashcode";
+
+export const result=[
+    {
+        id:0,
+        name:"",
+        location:"",
+        photo:"",
+        time:"",
+        seat:10,
+        restrict:"",
+        spaceUsage:[
+
+        ],
+        laptopUsage:LaptopUsage.LAPTIP_FALSE,
+        reserveUsage:ReserveUsage.RESERVE_FALSE,
+
+    }
+]
