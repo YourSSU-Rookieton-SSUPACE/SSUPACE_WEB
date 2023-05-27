f75c89f26815da44592b6a671a8ac93013ab1619
JjungminLee <julie0964@naver.com>
2023-05-27 21:37:45 +0900

    feat:조사보고 데이터 추가 완료

--- src/data/Hashcode.js
+++ src/data/Hashcode.js
@@ -9,9 +9,19 @@ export const SpaceUsage={
     
 };
 
+export const BuildingCategory={
+    H:"형남공학관",
+    W:"웨스트민스터홀",
+    I:"정보과학관",
+    S:"신양관",
+    L:"중앙도서관",
+    M:"미래관",
+    C:"조만식기념관",
+}
+
 export const LaptopUsage={
     LAPTOP_TRUE:"노트북 사용해도 돼요!",
-    LAPTIP_FALSE:"노트북 사용하면 안돼요!",
+    LAPTOP_FALSE:"노트북 사용하면 안돼요!",
 
 
 };
@@ -20,6 +30,11 @@ export const ReserveUsage={
     RESERVE_TRUE:"예약이 필요해요!",
     RESERVE_FALSE:"예약 안해도 돼요!"
 
+}
+
+export const DrinkAllow={
+    FALSE:"음료 반입 금지",
+    TRUE:"음료 반입 가능"
 }
 
 Object.freeze(SpaceUsage);
